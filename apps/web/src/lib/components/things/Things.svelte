4c60fe6014a9234da29c1bfb1e4e777b0ead73cf
Dillon Fagan <dillonfagan@pm.me>
2024-06-20 13:20:37 -0400

    Adjust responsive card layout

--- apps/web/src/lib/components/things/Things.svelte
+++ apps/web/src/lib/components/things/Things.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#key things}
-	<div class="grid grid-cols-3 md:grid-cols-5 gap-x-4 gap-y-6 place-content-between">
+	<div class="grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-6 place-content-between">
 		{#each things as thing}
 			<Thing {thing} />
 		{/each}
