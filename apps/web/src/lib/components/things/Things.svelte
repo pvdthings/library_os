5b60c149e5717fa84d5681ac6c0c334294e2b851
Dillon Fagan <dillonfagan@pm.me>
2024-06-20 11:13:03 -0400

    Remove unneeded div

--- apps/web/src/lib/components/things/Things.svelte
+++ apps/web/src/lib/components/things/Things.svelte
@@ -4,12 +4,10 @@
 	export let things;
 </script>
 
-<div>
-	{#key things}
-		<div class="grid grid-cols-3 md:grid-cols-5 gap-x-4 gap-y-6 place-content-between">
-			{#each things as thing}
-				<Thing {thing} />
-			{/each}
-		</div>
-	{/key}
-</div>
+{#key things}
+	<div class="grid grid-cols-3 md:grid-cols-5 gap-x-4 gap-y-6 place-content-between">
+		{#each things as thing}
+			<Thing {thing} />
+		{/each}
+	</div>
+{/key}
