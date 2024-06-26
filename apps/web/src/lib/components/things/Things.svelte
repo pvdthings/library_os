f30499be2eb03b35d7e37da6f1a46eb6af970198
Dillon Fagan <dillonfagan@pm.me>
2024-06-26 00:00:48 -0400

    Remove unnecessary key block

--- apps/web/src/lib/components/things/Things.svelte
+++ apps/web/src/lib/components/things/Things.svelte
@@ -4,10 +4,8 @@
 	export let things;
 </script>
 
-{#key things}
-	<div class="grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-6 place-content-between">
-		{#each things as thing}
-			<Thing {thing} />
-		{/each}
-	</div>
-{/key}
+<div class="grid grid-cols-2 md:grid-cols-6 gap-x-4 gap-y-6 place-content-between">
+	{#each things as thing}
+		<Thing {thing} />
+	{/each}
+</div>
