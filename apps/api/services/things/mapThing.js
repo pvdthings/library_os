6ecd15880dc4837024f50bdd72eaeb2ffda0aaad
Dillon Fagan <dillonfagan@pm.me>
2024-07-05 15:46:02 -0400

    Load availableDate from Next Due Back field

--- apps/api/services/things/mapThing.js
+++ apps/api/services/things/mapThing.js
@@ -1,11 +1,13 @@
 function mapThing(record) {
+  const available = Number(record.get('Available'));
+
   return {
       id: record.id,
       name: record.get('Name'),
       name_es: record.get('name_es'),
       stock: Number(record.get('Stock')),
-      available: Number(record.get('Available')),
-      availableDate: '7/4/24',
+      available,
+      availableDate: available > 0 ? undefined : record.get('Next Due Back'),
       images: record.get('Image')?.map(image => image.url) || [],
       categories: record.get('Category') || [],
       hidden: Boolean(record.get('Hidden'))
