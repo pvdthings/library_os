01acdeb2c68f5c2ceee754058cb49b40abb3dfdf
Dillon Fagan <dillonfagan@pm.me>
2024-07-24 23:35:51 -0400

    Refactor Search and TextInput

--- apps/web/src/lib/views/SearchInputView.svelte
+++ apps/web/src/lib/views/SearchInputView.svelte
@@ -2,7 +2,6 @@
 	import { TextInput } from '$lib/components';
 	import { t } from '$lib/language/translate';
 	import { searchFilter } from '$lib/stores/catalog';
-	import XMarkIcon from '$lib/icons/x-mark.svg';
 	import SearchIcon from '$lib/icons/magnifying-glass.svg';
 
 	const clearSearch = () => {
@@ -10,12 +9,10 @@
 	};
 </script>
 
-<div class="relative">
-	<img src={SearchIcon} alt="Search" class="w-6 h-6 absolute top-2.5 left-2 transform" />
-	<TextInput bind:value={$searchFilter} placeholder={$t('Input.Search')} />
-	{#if $searchFilter.length > 0}
-		<button class="btn btn-circle btn-sm btn-ghost absolute right-2 top-1.5" on:click={clearSearch}>
-			<img src={XMarkIcon} alt="Clear Search" class="w-5 h-5 absolute top-1.75 left-2.25 transform" />
-		</button>
-	{/if}
-</div>
+<TextInput
+	leadingIcon={SearchIcon}
+	bind:value={$searchFilter}
+	placeholder={$t('Input.Search')}
+	clear={$searchFilter.length > 0}
+	on:clear={clearSearch}
+/>
