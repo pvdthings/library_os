8df434c9f2e40b7683f82742520752b8166aac19
Dillon Fagan <dillonfagan@pm.me>
2024-07-27 13:17:48 -0400

    Adjust body styling for mobile

--- apps/web/src/lib/components/Shell/Body.svelte
+++ apps/web/src/lib/components/Shell/Body.svelte
@@ -1,3 +1,3 @@
-<main class="flex-grow -my-3 py-6 lg:py-8 px-3 lg:px-0 lg:mx-auto lg:w-3/4 overflow-x-hidden overflow-y-auto relative">
+<main class="flex-grow -my-3 pt-6 pb-24 lg:pt-8 px-3 lg:px-0 lg:mx-auto lg:w-3/4 overflow-x-hidden overflow-y-scroll relative">
   <slot />
 </main>
