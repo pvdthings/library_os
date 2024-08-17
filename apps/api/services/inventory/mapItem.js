251f5fef503e3a1f65d67b94bc06ee82b6065cbc
Dillon Fagan <dillonfagan@pm.me>
2024-08-17 15:06:49 -0400

    API: Include location in Item DTO

--- apps/api/services/inventory/mapItem.js
+++ apps/api/services/inventory/mapItem.js
@@ -15,6 +15,7 @@ function mapItem(record) {
       estimatedValue: record.get('Estimated Value'),
       eyeProtection: Boolean(record.get('Eye Protection')),
       condition: record.get('Condition'),
+      location: record.get('Location')?.[0],
       totalLoans: record.get('Total Loans'),
       images: record.get('Picture')?.map(image => image.thumbnails.large.url) || [],
       manuals: record.get('Manuals')?.map(manual => ({
