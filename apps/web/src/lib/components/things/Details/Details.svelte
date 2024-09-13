2884de851562c0da05d37e1a17a60c3a1ae7265b
Dillon Fagan <dillonfagan@pm.me>
2024-09-13 14:49:56 -0400

    Improved look of inventory items with available status, Updated system colors

--- apps/web/src/lib/components/things/Details/Details.svelte
+++ apps/web/src/lib/components/things/Details/Details.svelte
@@ -1,71 +1,77 @@
 <script>
-	import CategoryBadge from "./CategoryBadge.svelte";
-  import BoxIcon from '$lib/icons/box.svg';
-	import CloseButton from "$lib/components/CloseButton.svelte";
-	import { getShellContext } from "$lib/components/Shell/ShellContext";
+	import CategoryBadge from './CategoryBadge.svelte';
+	import BoxIcon from '$lib/icons/box.svg';
+	import CloseButton from '$lib/components/CloseButton.svelte';
+	import { getShellContext } from '$lib/components/Shell/ShellContext';
+	import InventoryItem from './InventoryItem.svelte';
 
-  export let name = 'Unnamed Thing';
-  export let imageUrl = undefined;
-  export let availableStock = 0;
-  export let totalStock = 0;
-  export let categories = [];
+  // TODO: Refactor to accept Thing ID, then load from API
 
-  const { drawer } = getShellContext();
+	export let name = 'Unnamed Thing';
+	export let imageUrl = undefined;
+	export let availableStock = 0;
+	export let totalStock = 0;
+	export let categories = [];
 
-  const inventory = [
-    {
-      number: 1146,
-      brand: 'DeWalt'
-    },
-    {
-      number: 1147,
-      brand: 'Milwaukee'
-    },
-    {
-      number: 1148,
-      brand: 'Ryobi'
-    }
-  ]; 
+	const { drawer } = getShellContext();
+
+	const inventory = [
+		{
+			number: 1146,
+			brand: 'DeWalt'
+		},
+		{
+			number: 1147,
+			brand: 'Milwaukee'
+		},
+		{
+			number: 1148,
+			brand: 'Ryobi'
+		}
+	];
 </script>
 
-<div class="p-4 flex flex-col w-80">
-  <section class="-mx-4 -mt-4 mb-4 h-64 border-b border-base-300 overflow-hidden relative shadow-sm">
-    <img src={imageUrl ?? BoxIcon} alt={name} class="object-center object-contain bg-white h-full w-full" />
-    <CloseButton class="absolute top-4 right-4" on:click={drawer.close} />
-  </section>
-  <section class="flex flex-col gap-2">
-    <div class="font-display font-semibold text-2xl">{name}</div>
-    <div class="badge badge-success badge-lg">{availableStock} / {totalStock}</div>
-  </section>
-  <div class="divider" />
-  <section>
-    <div class="section-title">Categories</div>
-    <div class="flex flex-wrap gap-2">
-      {#if categories.length}
-        {#each categories as category}
-          <CategoryBadge text={category} />
-        {/each}
-      {:else}
-        <div>None</div>
-      {/if}
-    </div>
-  </section>
-  <div class="divider" />
-  <section>
-    <div class="section-title">Inventory</div>
-    <div class="flex flex-col gap-2">
-      {#each inventory as item}
-        <div class="flex flex-row gap-2">
-          <div>#{item.number}</div>
-          <div>{item.brand}</div>
-        </div>
-      {/each}
-    </div>
-  </section>
+<div class="w-80">
+	<section class="h-64 border-b border-base-300 overflow-hidden relative shadow-sm">
+		<img
+			src={imageUrl ?? BoxIcon}
+			alt={name}
+			class="object-center object-contain bg-white h-full w-full"
+		/>
+		<CloseButton class="absolute top-4 right-4" on:click={drawer.close} />
+	</section>
+	<div class="p-4 flex flex-col overflow-y-scroll">
+		<section class="flex flex-col gap-2">
+			<div class="font-display font-semibold text-2xl">{name}</div>
+			<div class="badge badge-success badge-lg">{availableStock} / {totalStock}</div>
+		</section>
+		<div class="divider" />
+		<section>
+			<div class="section-title">Categories</div>
+			<div class="flex flex-wrap gap-2">
+				{#if categories.length}
+					{#each categories as category}
+						<CategoryBadge text={category} />
+					{/each}
+				{:else}
+					<div>None</div>
+				{/if}
+			</div>
+		</section>
+		<div class="divider" />
+		<section>
+			<div class="section-title">Inventory</div>
+			<div class="flex flex-col gap-2">
+				{#each inventory as item}
+					<InventoryItem number={item.number} {imageUrl} brand={item.brand} available={false} />
+				{/each}
+			</div>
+		</section>
+	</div>
 </div>
 
 <style>
-  .section-title {
-    @apply font-display font-semibold text-xl mb-2;
-  }
+	.section-title {
+		@apply font-display font-semibold text-xl mb-2;
+	}
 </style>
