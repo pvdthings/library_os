f12f37c1a4ee591bea41d55c7efb1e0f2e934245
Dillon Fagan <dillonfagan@pm.me>
2024-11-18 16:57:18 -0500

    WIP with hardcoded data

---
+++ apps/web/src/lib/shifts/ShiftCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let selected: boolean = false;
+	export let title: string;
+  export let date: string;
+  export let volunteers: string[];
+</script>
+
+<div class="card bg-white border border-base-300 p-4 lg:p-8 shadow-sm">
+	<div class="grid grid-cols-2 mb-4 lg:mb-6">
+		<div>
+			<div class="mb-1 font-display font-semibold">{date}</div>
+			<div class="font-display text-xl lg:text-2xl">{title}</div>
+		</div>
+		<div class="flex justify-end">
+      {#if selected}
+      <button class="font-display btn btn-sm lg:btn-md btn-success border border-success shadow-sm">
+        <span class="ph-bold ph-check text-xl" />
+        <span>Added</span>
+      </button>
+      {:else}
+        <button class="font-display btn btn-sm lg:btn-md btn-secondary border border-base-300 shadow-sm">
+          <span class="ph-bold ph-user-plus text-xl" />
+          <span>Add me</span>
+        </button>
+      {/if}
+		</div>
+	</div>
+
+	<div>
+		<div class="mb-1 font-display font-semibold text-sm lg:text-base">Volunteers</div>
+		<div class="flex flex-wrap gap-2">
+      {#each volunteers as volunteer}
+			  <div class="badge badge-ghost lg:p-4 lg:text-lg">{volunteer}</div>
+      {/each}
+		</div>
+	</div>
+</div>
---
+++ apps/web/src/routes/shifts/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import ShiftCard from '$lib/shifts/ShiftCard.svelte';
+
+	const shifts = [
+		{
+			id: '0',
+			date: 'February 24, 2024',
+			title: 'Saturday Librarian Shift',
+			volunteers: ['Alice', 'Bob']
+		},
+		{
+			id: '1',
+			date: 'March 2, 2024',
+			title: 'Tuesday Mechanic Shift',
+			volunteers: ['Bob', 'Charlie']
+		},
+		{
+			id: '2',
+			date: 'March 3, 2024',
+			title: 'Wednesday Librarian Shift',
+			volunteers: ['Alice']
+		}
+	];
+
+  const selectedShifts = ['1'];
+</script>
+
+<div class="flex flex-col gap-4">
+	{#each shifts as shift}
+		<ShiftCard selected={selectedShifts.includes(shift.id)} date={shift.date} title={shift.title} volunteers={shift.volunteers} />
+	{/each}
+	<div class="font-display py-4 text-xl text-center">
+    <span class="font-semibold">{selectedShifts.length} shifts</span> are assigned to me.
+  </div>
+	<button class="btn btn-lg btn-primary shadow">Confirm</button>
+</div>
+
+<div class="toast toast-center toast-end">
+	<div class="alert alert-info shadow-lg">
+		<span class="ph-bold ph-warning" />
+		<span class="font-display">Unsaved changes</span>
+	</div>
+</div>
