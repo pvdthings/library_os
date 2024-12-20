92c118525f1657ad12438131838e9033d5398b6d
Dillon Fagan <dillonfagan@pm.me>
2024-12-19 21:30:47 -0500

    remove logging of env vars

--- apps/web/src/lib/supabaseClient.ts
+++ apps/web/src/lib/supabaseClient.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_KEY } from "$env/static/private";
 
-console.log(SUPABASE_URL);
-console.log(SUPABASE_KEY);
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
