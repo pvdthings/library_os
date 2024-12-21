fe6e173ca36ddd32bbf060b49a60206c28e97d3b
Dillon Fagan <dillonfagan@pm.me>
2024-12-20 20:19:15 -0500

    load availability in item details

--- apps/web/src/routes/api/items/[id]/+server.ts
+++ apps/web/src/routes/api/items/[id]/+server.ts
@@ -5,31 +5,32 @@ export const GET = async ({ params }) => {
   const { id } = params;
   const { data: items } = await supabase.from('items')
     .select(`
-      id,
-      brand,
-      number,
-      status,
-      item_attachments ( url ),
-      item_images ( url ),
-      things ( id, eye_protection, name, spanish_name )
+      *,
+      attachments:item_attachments ( url ),
+      images:item_images ( url ),
+      things ( id, eye_protection, name, spanish_name ),
+      loans:loans_items (
+        *,
+        loan_details:loans ( * )
+      )
     `)
-    .eq('id', id);
+    .eq('id', id)
+    .eq('loans_items.returned', false);
 
   const item = items?.[0];
   const { eye_protection, name, spanish_name } = item.things;
-
-  console.log('item', item);
+  const hasActiveLoan = !!item.loans.length;
 
   return json({
     id: item.id,
-    available: true, // TODO
-    availableDate: undefined, // TODO
+    available: !hasActiveLoan,
+    availableDate: item.loans.length ? item.loans[0].loan_details?.due_date : undefined,
     brand: item.brand,
     status: item.status,
     eyeProtection: eye_protection,
-    image: item.item_images.length ? item.item_images[0].url : undefined,
+    image: item.images.length ? item.images[0].url : undefined,
     number: item.number,
-    manuals: item.item_attachments.map(a => a.url),
+    manuals: item.attachments.map(a => a.url),
     name: name,
     spanishName: spanish_name
   });
