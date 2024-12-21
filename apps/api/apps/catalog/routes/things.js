0b135bc1fdbc8d302bd873211d2c60cc6a8e6af3
Dillon Fagan <dillonfagan@pm.me>
2024-12-20 21:09:01 -0500

    remove unused routes from catalog API

--- apps/api/apps/catalog/routes/things.js
+++ apps/api/apps/catalog/routes/things.js
@@ -1,39 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getCatalogData } = require('../services/catalog');
-const { getThingDetails } = require('../services/thingDetails');
-const { getItemDetails } = require('../services/itemDetails');
 const { enroll, getShifts } = require('../services/shifts');
 const { findMember } = require('../../../services/borrowers');
-
-router.get('/', async (req, res) => {
-    try {
-        res.send(await getCatalogData());
-    } catch (error) {
-        console.error(error);
-        res.status(error.status || 500).send({ errors: [error] });
-    }
-});
-
-router.get('/things/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        res.send(await getThingDetails(id));
-    } catch (error) {
-        console.error(error);
-        res.status(error.status || 500).send({ errors: [error] });
-    }
-});
-
-router.get('/items/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        res.send(await getItemDetails(id));
-    } catch (error) {
-        console.error(error);
-        res.status(error.status || 500).send({ errors: [error] });
-    }
-});
 
 router.get('/volunteer/shifts', async (req, res) => {
     const email = req.headers['x-email'];
--- apps/api/apps/catalog/services/mapItemStatus.js
+++
@@ -1,9 +0,0 @@
-function mapItemStatus(item) {
-  if (item.available) {
-    return 'available';
-  }
-
-  return 'checkedOut';
-}
-
-module.exports = mapItemStatus;
