788edcbcef0561f5b892c5775c89569ca207abf4
Dillon Fagan <dillonfagan@pm.me>
2024-12-20 19:07:11 -0500

    load thing availability info

--- apps/web/src/routes/(app)/+page.server.ts
+++ apps/web/src/routes/(app)/+page.server.ts
@@ -2,29 +2,45 @@ import type { AppData } from '$lib/models/AppData';
 import { supabase } from '$lib/supabaseClient.js';
 
 export const load = async (): Promise<AppData> => {
-  const { data: categories, error: categoriesError } = await supabase.from('categories').select('name');
-  const { data: things } = await supabase.from('things').select(`
-    id,
-    name,
-    spanish_name,
-    categories ( name ),
-    thing_images ( url )
-  `);
+  const { data: categories, error: categoriesError } = await supabase
+    .from('categories')
+    .select('name');
+
+  const { data: things, error: thingsError } = await supabase
+    .from('things')
+    .select(`
+      *,
+      categories ( name ),
+      images:thing_images ( url ),
+      items (
+        stock:count
+      ),
+      loans:loans_items (
+        unavailable:count
+      )
+    `).eq('loans.returned', false);
 
   if (categoriesError) {
     console.log('error', categoriesError);
   }
 
+  if (thingsError) {
+    console.log('error', thingsError);
+  }
+
   return {
     categories: ['All', ...categories.map(c => c.name)],
-    things: things.map(thing => ({
-      id: thing.id,
-      name: thing.name,
-      image: thing.thing_images.length ? thing.thing_images[0].url : undefined,
-      spanishName: thing.spanish_name,
-      categories: thing.categories.map(cat => cat.name),
-      stock: 1,
-      available: 1
-    }))
+    things: things.map(thing => {
+      const { categories, images, items: [{ stock }], loans: [{ unavailable }] } = thing;
+      return {
+        id: thing.id,
+        name: thing.name,
+        image: images.length ? images[0].url : undefined,
+        spanishName: thing.spanish_name,
+        categories: categories.map(c => c.name),
+        stock,
+        available: stock - unavailable
+      };
+    })
   };
 };
