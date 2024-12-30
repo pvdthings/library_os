1abbb8db9f5801ef447e8d3fc5e853fd8824df9e
Dillon Fagan <dillonfagan@pm.me>
2024-12-30 10:31:25 -0500

    remove unused loans API code

--- apps/librarian/lib/core/api/dto.dart
+++ apps/librarian/lib/core/api/dto.dart
@@ -6,35 +6,3 @@ class ImageDTO {
 
   const ImageDTO({required this.url, this.name});
 }
-
-class NewLoan {
-  String borrowerId;
-  List<String> thingIds;
-  String checkedOutDate;
-  String dueBackDate;
-  String? notes;
-
-  NewLoan({
-    required this.borrowerId,
-    required this.thingIds,
-    required this.checkedOutDate,
-    required this.dueBackDate,
-    this.notes,
-  });
-}
-
-class UpdatedLoan {
-  String loanId;
-  String thingId;
-  String? dueBackDate;
-  String? checkedInDate;
-  String? notes;
-
-  UpdatedLoan({
-    required this.loanId,
-    required this.thingId,
-    this.dueBackDate,
-    this.checkedInDate,
-    this.notes,
-  });
-}
--- apps/librarian/lib/core/api/loans.dart
+++ apps/librarian/lib/core/api/loans.dart
@@ -1,28 +1,5 @@
 part of 'api.dart';
 
-Future<Response> createLoan(NewLoan data) async {
-  return await DioClient.instance.put('/loans', data: {
-    'borrowerId': data.borrowerId,
-    'thingIds': data.thingIds,
-    'checkedOutDate': data.checkedOutDate,
-    'dueBackDate': data.dueBackDate
-  });
-}
-
-Future<Response> updateLoan(UpdatedLoan loanData) async {
-  dynamic data = {
-    'checkedInDate': loanData.checkedInDate,
-    'dueBackDate': loanData.dueBackDate,
-  };
-
-  if (loanData.notes != null) {
-    data['notes'] = loanData.notes;
-  }
-
-  return await DioClient.instance
-      .patch('/loans/${loanData.loanId}/${loanData.thingId}', data: data);
-}
-
 Future<Response> extendAuthorization() async {
   return await DioClient.instance.head('/loans/extend');
 }
