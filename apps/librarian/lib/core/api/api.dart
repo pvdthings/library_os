3cd8b1ca5f9cbcee51ca44e79a1ebe5da19c3ca9
Dillon Fagan <dillonfagan@pm.me>
2025-01-10 19:45:09 -0500

    remove unused inventory and things apis

--- apps/librarian/lib/core/api/api.dart
+++ apps/librarian/lib/core/api/api.dart
@@ -7,8 +7,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 part 'borrowers.dart';
 part 'dio_client.dart';
 part 'dto.dart';
-part 'inventory.dart';
 part 'loans.dart';
 part 'messages.dart';
 part 'payments.dart';
-part 'things.dart';
