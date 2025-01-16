e30a40be52eb8a7e70c8e5f17a07f6025d4ca6d4
Dillon Fagan <dillonfagan@pm.me>
2025-01-15 21:06:42 -0500

    root server adjustments

--- apps/api/server.js
+++ apps/api/server.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 
 const { isDevelopment } = require('./utils/environment');
 const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const buildDocOptions = require('./docs/scripts/buildOptions');
-const swaggerUi = require('swagger-ui-express');
 const app = express();
 const bodyParser = require('body-parser');
-// const auth = require('./auth');
 const things = require('./apps/catalog/routes/things');
 const lending = require('./apps/librarian');
 const cors = require('cors');
@@ -28,10 +24,11 @@ const corsOptions = Object.freeze({
     ],
     credentials: true,
     origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
+        if (allowedOrigins.includes(origin) || !origin || isDevelopment()) {
             callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            console.log('origin', origin);
+            callback(new Error('CORS Error'));
         }
     }
 });
@@ -44,27 +41,14 @@ app.use(bodyParser.json());
 app.use('/web', things);
 app.use('/things', things);
 app.use('/lending', lending);
-// app.use('/auth', auth);
 
 app.use((req, res, next) => {
     res.status(404).send();
 });
 
-const PORT = process.env.PORT || 8088;
-
-if (isDevelopment()) {
-    const docOptions = buildDocOptions({ port: PORT });
-    const specs = swaggerJsDoc(docOptions);
-
-    app.use(
-        '/docs',
-        swaggerUi.serve,
-        swaggerUi.setup(specs, { explorer: true })
-    );
-}
-
 app.disable('x-powered-by');
 
+const PORT = process.env.PORT || 8088;
 app.listen(PORT, () => {
-    console.log(`PVD Things API listening on PORT ${PORT}...`);
+    console.log(`Server is running on port ${PORT}...`);
 });
