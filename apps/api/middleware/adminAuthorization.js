675cb08ee138b668c61087665a4909c00a27178b
Dillon Fagan <dillonfagan@pm.me>
2025-01-16 19:21:59 -0500

    authorize supabase user in api

--- apps/api/middleware/adminAuthorization.js
+++ apps/api/middleware/adminAuthorization.js
@@ -2,7 +2,7 @@ const { isAdmin } = require('../services/auth');
 
 const authorizeAdminUser = async (req, res, next) => {
   const adminWhitelist = process.env.ADMIN_WHITELIST;
-  const userEmail = res.locals.user?.email;
+  const userEmail = req.user?.email;
 
   if (adminWhitelist && (!userEmail || !isAdmin(userEmail))) {
     res.status(403).send({ error: 'Not authorized' });
