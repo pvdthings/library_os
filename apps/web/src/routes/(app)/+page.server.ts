99d2eb234f063d1930c46336c062151fa17e35cb
Dillon Fagan <dillonfagan@pm.me>
2025-01-18 23:07:24 -0500

    web: move catalog load to page server

--- apps/web/src/routes/(app)/+page.server.ts
+++ apps/web/src/routes/(app)/+page.server.ts
@@ -1,6 +1,12 @@
 import type { AppData } from '$lib/api/models/AppData';
-import { fetchThings } from '$lib/api/api.js'
+import { API_HOST, API_KEY } from '$env/static/private';
 
 export const load = async ({ fetch }): Promise<AppData> => {
-  return fetchThings(fetch);
+  const result = await fetch(`${API_HOST}/web/catalog`, {
+    headers: {
+      'x-api-key': API_KEY
+    }
+  });
+
+  return await result.json();
 }
