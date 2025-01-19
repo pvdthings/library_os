224375882fcbc7582560cb926c575f1d97875da8
Dillon Fagan <dillonfagan@pm.me>
2025-01-18 22:33:57 -0500

    web: move models into api folder

--- apps/web/src/lib/api/api.ts
+++ apps/web/src/lib/api/api.ts
@@ -1,5 +1,5 @@
 import { API_HOST, API_KEY } from "$env/static/private";
-import type { AppData } from "$lib/models/AppData";
+import type { AppData } from "$lib/api/models/AppData";
 
 export const fetchThings = async (fetch): Promise<AppData> => {
   const result = await fetch(`${API_HOST}/web/things`, {
--- apps/web/src/routes/(app)/+page.server.ts
+++ apps/web/src/routes/(app)/+page.server.ts
@@ -1,5 +1,5 @@
-import type { AppData } from '$lib/models/AppData';
-import { fetchThings } from '$lib/server/api.js'
+import type { AppData } from '$lib/api/models/AppData';
+import { fetchThings } from '$lib/api/api.js'
 
 export const load = async ({ fetch }): Promise<AppData> => {
   return fetchThings(fetch);
