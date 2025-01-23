8ec6557493b11ae38ebc608eda7264c7360b29fc
Dillon Fagan <dillonfagan@pm.me>
2025-01-23 08:50:08 -0500

    librarian: remove dotenv package

--- apps/librarian/lib/constants.dart
+++ apps/librarian/lib/constants.dart
@@ -1,24 +1,10 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-Future<void> loadEnvironmentVariables() async {
-  await dotenv.load(isOptional: true);
-}
-
 const supabaseUrlKey = 'SUPABASE_URL';
 const supabasePublicKeyKey = 'SUPABASE_PUBLIC_KEY';
 const apiHostKey = 'API_HOST';
 const apiKeyKey = 'API_KEY';
 const appUrlKey = 'APP_URL';
 
-const String _supabaseUrl = String.fromEnvironment(supabaseUrlKey);
-const String _supabasePublicKey = String.fromEnvironment(supabasePublicKeyKey);
-const String _apiHost = String.fromEnvironment(apiHostKey);
-const String _apiKey = String.fromEnvironment(apiKeyKey);
-const String _appUrl = String.fromEnvironment(appUrlKey);
-
-String get supabaseUrl => dotenv.get(supabaseUrlKey, fallback: _supabaseUrl);
-String get supabasePublicKey =>
-    dotenv.get(supabasePublicKeyKey, fallback: _supabasePublicKey);
-String get apiHost => dotenv.get(apiHostKey, fallback: _apiHost);
-String get apiKey => dotenv.get(apiKeyKey, fallback: _apiKey);
-String get appUrl => dotenv.get(appUrlKey, fallback: _appUrl);
+const apiHost = String.fromEnvironment(apiHostKey);
+const apiKey = String.fromEnvironment(apiKeyKey);
+const supabaseUrl = String.fromEnvironment(supabaseUrlKey);
+const supabasePublicKey = String.fromEnvironment(supabasePublicKeyKey);
--- apps/librarian/lib/main.dart
+++ apps/librarian/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' as riverpod;
-import 'package:librarian_app/constants.dart';
 import 'package:librarian_app/core/supabase.dart';
 import 'package:librarian_app/modules/splash/pages/splash_page.dart';
 import 'package:librarian_app/theme/indigo_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await loadEnvironmentVariables();
   initializeSupabase();
 
   runApp(const riverpod.ProviderScope(
