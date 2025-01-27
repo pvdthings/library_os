552438b79310cd2378be40a91223b9022337e5d6
Dillon Fagan <dillonfagan@pm.me>
2025-01-27 18:20:49 -0500

    remove unused auth code

--- apps/api/auth/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const checkEndpoint = require('./routes/check');
-
-router.use('/check', checkEndpoint);
-
-module.exports = router;
