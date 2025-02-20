6f1cbeb2c55c394a1b4542db2afeadc51bba6e32
Dillon Fagan <dillonfagan@pm.me>
2025-02-19 21:23:43 -0500

    librarian: remove dio client

--- apps/librarian/lib/constants.dart
+++ apps/librarian/lib/constants.dart
@@ -1,10 +1,5 @@
 const supabaseUrlKey = 'SUPABASE_URL';
 const supabasePublicKeyKey = 'SUPABASE_PUBLIC_KEY';
-const apiHostKey = 'API_HOST';
-const apiKeyKey = 'API_KEY';
-const appUrlKey = 'APP_URL';
 
-const apiHost = String.fromEnvironment(apiHostKey);
-const apiKey = String.fromEnvironment(apiKeyKey);
 const supabaseUrl = String.fromEnvironment(supabaseUrlKey);
 const supabasePublicKey = String.fromEnvironment(supabasePublicKeyKey);
--- apps/librarian/lib/core/api/api.dart
+++ apps/librarian/lib/core/api/api.dart
@@ -1,8 +1,3 @@
 library;
 
-import 'package:dio/dio.dart';
-import 'package:librarian_app/constants.dart';
-import 'package:librarian_app/core/supabase.dart';
-
-part 'dio_client.dart';
 part 'dto.dart';
