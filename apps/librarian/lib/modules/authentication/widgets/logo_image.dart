d1162db44c52cfe584bcdc4a4a8082a3c42259cc
Dillon Fagan <dillonfagan@pm.me>
2026-03-01 11:13:57 -0500

    Display default icon if logo cannot be loaded.

--- apps/librarian/lib/modules/authentication/widgets/logo_image.dart
+++ apps/librarian/lib/modules/authentication/widgets/logo_image.dart
@@ -1,13 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:librarian_app/core/core.dart';
 
 class LogoImage extends StatelessWidget {
+  const LogoImage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final defaultIcon = const Icon(
+      Icons.local_library_outlined,
+      size: 120,
+    );
+
     if (Library.logoUrl != null) {
       return Image.network(
         Library.logoUrl!,
+        errorBuilder: (context, error, stackTrace) {
+          return defaultIcon;
+        },
         loadingBuilder: (context, child, progress) {
           return Center(child: child);
         },
@@ -16,17 +25,6 @@ class LogoImage extends StatelessWidget {
       );
     }
 
-    if (kDebugMode) {
-      return Image.asset(
-        'pvd_things.png',
-        isAntiAlias: true,
-        height: 120,
-      );
-    }
-
-    return const Icon(
-      Icons.local_library_outlined,
-      size: 120,
-    );
+    return defaultIcon;
   }
 }
